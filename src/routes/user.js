33e7d9af3ca41754549f29842e3d5e4a1614c747
More <moelvime@gmal.com>
2023-05-28 06:59:20 -0400

    Finish

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,51 @@
-const express = require("express")
+const express = require("express");
+const userSchema = require ("../models/user");
 
 const router=express.Router();
 
-//creater user
+//creater user 
 router.post('/users', (req, res)=>{
-    res.send('Create User');
+    const user = userSchema(req.body);
+        user 
+            .save()
+            .then((data)=>res.json(data))
+            .catch((error)=>res.json({message: error}));
 });
 
-module.export= router;
+//get all usert
+router.get('/users', (req, res)=>{
+            userSchema 
+            .find()
+            .then((data)=>res.json(data))
+            .catch((error)=>res.json({message: error}));
+});
+
+//get a user
+router.get('/users/:id', (req, res)=>{
+const{id}=req.params;
+    userSchema 
+    .findById(id)
+    .then((data)=>res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
+   
+//uptdate a user
+router.put('/users/:id', (req, res)=>{
+    const{ id } = req.params;
+    const{ name, age, email }= req.body;
+        userSchema 
+        .updateOne({ _id: id }, { $set: ( name, age, email )})
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message: error}));
+    });
+       
+//delete a user
+router.delete('/users/:id', (req, res)=>{
+    const{ id } = req.params;
+        userSchema 
+        .remove({ _id: id })
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message: error}));
+    });
+
+module.exports= router;
